9b6cb2a4d9e5136b5975a8f050083ff4d6523f3f
Pavel <pavelfedorov929@gmail.com>
2021-01-28 21:57:20 +0300

    commited

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const AppHeader = () => {
+    const margin = 40;
+    const headerStyle = {
+        color: 'red',
+        marginLeft: `${margin}px`,
+        marginBottom: `${margin}px`,
+    }
+    return(
+        <h1
+        //style={headerStyle}
+        className="header"
+        >
+            Hello React
+        </h1>
+    );
+}
+
+const AppList = () => {
+    const items = ['item 1', 'item 2', 'item 3'];
+    return(
+        <ul>
+            {
+                items.map(item => <li>{item}</li>)
+            }
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    );
+}
+
+const App = () => {
+    return(
+        <React.Fragment>
+            <AppHeader />
+            <AppList />
+            <AppHeader />
+            <AppList />
+        </React.Fragment>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
